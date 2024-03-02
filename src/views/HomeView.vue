41ea58124dd15f6b8c08e48d99fcef5846ddd6c2
unknown <martinaperesa11@gmail.com>
2024-03-02 20:10:32 +0100

    Edit files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,11 @@
  <div class="row">
  <div class="col-1"></div>
  <div class="col-2">
- FILTER RESULTS
+ <hello-world />
+ <hello-world />
  </div>
  <div class="col-8">
+  <hello-world />
     <div class="card text-center">
   <div class="card-header">
     <ul class="nav nav-tabs card-header-tabs">
@@ -12,18 +14,17 @@
         <a class="nav-link active" href="#">Active</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Adopted</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      
     </ul>
   </div>
-  <div class="card-body">
+  <div class="card-body p-0">
     <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <img class="card-img-top" src="@/assets/dog2.jpg" />
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
+  
 </div>
  </div>
  </div>
@@ -31,7 +32,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/listing.vue'
 
 export default {
   name: 'HomeView',
