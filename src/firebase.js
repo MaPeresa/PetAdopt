48901c5856a078a072b09f9afcb7e190e1c0d935
unknown <martinaperesa11@gmail.com>
2024-03-20 02:58:21 +0100

    Dodavanje firebasea

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyClkg6ocKUSAqqU5lN0EofM8Y4XkJXYN9o",
+    authDomain: "petadopt-42aed.firebaseapp.com",
+    projectId: "petadopt-42aed",
+    storageBucket: "petadopt-42aed.appspot.com",
+    messagingSenderId: "78586776627",
+    appId: "1:78586776627:web:363be6ea0fdb2e409ee746",
+  };
+  
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
